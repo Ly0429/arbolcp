1164d9fb393378dd2e3e3653ad51891ae9c2cb8b
Ly0429 <lely.r.04.29@gmail.com>
2026-05-04 23:09:48 -0500

    first commit

---
+++ arbolnuevo.js
@@ -0,0 +1,24 @@
+const lienzo = document.getElementById("lienzo");
+const ctx = lienzo.getContext("2d");
+
+function dibujarRama(x, y, largo, angulo, nivel) {
+    if (nivel === 0) {
+        return;
+    }
+
+    const xFinal = x + largo * Math.cos(angulo);
+    const yFinal = y + largo * Math.sin(angulo);
+
+    ctx.beginPath();
+    ctx.moveTo(x, y);
+    ctx.lineTo(xFinal, yFinal);
+    ctx.strokeStyle = "#0A0A0A"; //color
+    ctx.lineWidth = nivel;
+    ctx.stroke();
+
+    dibujarRama(xFinal, yFinal, largo * 0.75, angulo - 0.5, nivel - 1);
+    dibujarRama(xFinal, yFinal, largo * 0.75, angulo + 0.5, nivel - 1);
+    dibujarRama(xFinal, yFinal, largo * 0.75, angulo, nivel - 1);
+}
+
+dibujarRama(450, 550, 120, -Math.PI / 2, 10); //Aqui es lo de los niveles
